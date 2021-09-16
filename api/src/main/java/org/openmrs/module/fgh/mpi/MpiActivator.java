4aa2cd23c59137b42f19ecbd4a119977752dcc49
wluyima <wyclif@openmrs.org>
2021-09-16 00:56:52 -0500

    EC-75 - Added daily rolling file appender to log debezium and mpi module logs to special file

--- api/src/main/java/org/openmrs/module/fgh/mpi/MpiActivator.java
+++ api/src/main/java/org/openmrs/module/fgh/mpi/MpiActivator.java
@@ -9,9 +9,16 @@
  */
 package org.openmrs.module.fgh.mpi;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.DailyRollingFileAppender;
 import org.apache.log4j.PatternLayout;
+import org.openmrs.api.APIException;
 import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.util.OpenmrsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,18 +26,35 @@ public class MpiActivator extends BaseModuleActivator {
 	
 	private static final Logger log = LoggerFactory.getLogger(MpiActivator.class);
 	
+	private static final String MPI_APPENDER_NAME = "MPI_APPENDER";
+	
 	/**
 	 * @see BaseModuleActivator#started()
 	 */
 	@Override
 	public void started() {
 		log.info("MPI module started");
+		log.info("Registering MPI log appender");
 		
 		org.apache.log4j.Logger rootLogger = org.apache.log4j.Logger.getRootLogger();
 		ConsoleAppender consoleAppender = (ConsoleAppender) rootLogger.getAppender("CONSOLE");
-		PatternLayout patternLayout = new PatternLayout("%-5p %t - %C{1}.%M(%L) |%d{ISO8601}| %m%n");
-		consoleAppender.setLayout(patternLayout);
+		PatternLayout layout = new PatternLayout("%-5p %t - %C{1}.%M(%L) |%d{ISO8601}| %m%n");
+		consoleAppender.setLayout(layout);
 		consoleAppender.activateOptions();
+		
+		File mpiLogFile = Paths.get(OpenmrsUtil.getApplicationDataDirectory(), "mpi", "logs", "mpi.log").toFile();
+		try {
+			DailyRollingFileAppender mpiAppender = new DailyRollingFileAppender(layout, mpiLogFile.getAbsolutePath(),
+			        "'.'yyyy-MM-dd");
+			mpiAppender.setName(MPI_APPENDER_NAME);
+			rootLogger.addAppender(mpiAppender);
+			org.apache.log4j.Logger.getLogger("org.openmrs.module.debezium").addAppender(mpiAppender);
+			org.apache.log4j.Logger.getLogger("org.openmrs.module.fgh.mpi").addAppender(mpiAppender);
+		}
+		catch (IOException e) {
+			throw new APIException(e);
+		}
+		
 	}
 	
 	/**
@@ -39,6 +63,12 @@ public class MpiActivator extends BaseModuleActivator {
 	@Override
 	public void stopped() {
 		log.info("MPI module stopped");
+		log.info("Removing MPI log appender");
+		
+		org.apache.log4j.Logger rootLogger = org.apache.log4j.Logger.getRootLogger();
+		if (rootLogger.getAppender(MPI_APPENDER_NAME) != null) {
+			rootLogger.removeAppender(MPI_APPENDER_NAME);
+		}
 	}
 	
 }
