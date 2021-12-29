3ee3a2b3679229e07a1dea0683aa29a97c93d06d
wluyima <wyclif@openmrs.org>
2021-12-29 15:56:20 +0300

    EC-81 - Added address and phone fields to test that ensures that fhir patient map is generated

--- api/src/test/java/org/openmrs/module/fgh/mpi/FhirUtilsTest.java
+++ api/src/test/java/org/openmrs/module/fgh/mpi/FhirUtilsTest.java
@@ -1,11 +1,18 @@
 package org.openmrs.module.fgh.mpi;
 
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
+import static org.openmrs.module.fgh.mpi.FhirUtils.ATTR_QUERY;
+import static org.openmrs.module.fgh.mpi.FhirUtils.ATTR_TYPE_ID_PLACEHOLDER;
+import static org.openmrs.module.fgh.mpi.MpiConstants.DATETIME_FORMATTER;
 import static org.openmrs.module.fgh.mpi.MpiIntegrationProcessor.ID_PLACEHOLDER;
+import static org.openmrs.module.fgh.mpi.MpiUtils.executeQuery;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -43,61 +50,89 @@ public class FhirUtilsTest {
 	@Test
 	public void buildPatient_shouldBuildAPatientResource() throws Exception {
 		final String patientId = "1";
-		List<Object> personDetails = new ArrayList();
 		final String birthDate = "1986-10-07";
 		final boolean dead = false;
 		final String patientUuid = "person-uuid";
 		final boolean personVoided = false;
 		final boolean patientVoided = false;
+		List<Object> personDetails = Arrays.asList("M", birthDate, dead, null, patientUuid, personVoided);
 		final String identifier1 = "12345";
 		final String idTypeUuid1 = "id-type-uuid-1";
-		final String idUuid = "id-uuid-1";
-		personDetails.add("M");
-		personDetails.add(birthDate);
-		personDetails.add(dead);
-		personDetails.add(null);
-		personDetails.add(patientUuid);
-		personDetails.add(personVoided);
-		List<Object> id1 = new ArrayList();
-		id1.add(identifier1);
-		id1.add(idTypeUuid1);
-		id1.add(idUuid);
-		List<Object> id2 = new ArrayList();
-		id2.add("qwerty");
-		id2.add("id-type-uuid-2");
-		id2.add("id-uuid-2");
-		List<List<Object>> ids = new ArrayList();
-		ids.add(id1);
-		ids.add(id2);
-		when(MpiUtils.executeQuery(FhirUtils.ID_QUERY.replace(ID_PLACEHOLDER, patientId))).thenReturn(ids);
+		final String idUuid1 = "id-uuid-1";
+		List<Object> id1 = Arrays.asList(identifier1, idTypeUuid1, idUuid1);
+		final String identifier2 = "qwerty";
+		final String idTypeUuid2 = "id-type-uuid-2";
+		final String idUuid2 = "id-uuid-2";
+		List<Object> id2 = Arrays.asList(identifier2, idTypeUuid2, idUuid2);
+		List<List<Object>> ids = Arrays.asList(id1, id2);
+		when(executeQuery(FhirUtils.ID_QUERY.replace(ID_PLACEHOLDER, patientId))).thenReturn(ids);
 		final String prefix1 = "Mr";
 		final String givenName1 = "Horatio";
 		final String middleName1 = "D";
 		final String familyName1 = "HornBlower";
 		final String nameUuid1 = "name-uuid-1";
+		List<Object> name1 = Arrays.asList(prefix1, givenName1, middleName1, familyName1, nameUuid1);
 		final String prefix2 = "Miss";
 		final String givenName2 = "John";
 		final String middleName2 = "S";
 		final String familyName2 = "Doe";
 		final String nameUuid2 = "name-uuid-2";
-		List<Object> name1 = new ArrayList();
-		name1.add(prefix1);
-		name1.add(givenName1);
-		name1.add(middleName1);
-		name1.add(familyName1);
-		name1.add(nameUuid1);
-		List<Object> name2 = new ArrayList();
-		name2.add(prefix2);
-		name2.add(givenName2);
-		name2.add(middleName2);
-		name2.add(familyName2);
-		name2.add(nameUuid2);
-		List<List<Object>> names = new ArrayList();
-		names.add(name1);
-		names.add(name2);
-		when(MpiUtils.executeQuery(FhirUtils.NAME_QUERY.replace(ID_PLACEHOLDER, patientId))).thenReturn(names);
+		List<Object> name2 = Arrays.asList(prefix2, givenName2, middleName2, familyName2, nameUuid2);
+		List<List<Object>> names = Arrays.asList(name1, name2);
+		when(executeQuery(FhirUtils.NAME_QUERY.replace(ID_PLACEHOLDER, patientId))).thenReturn(names);
+		final String line1Address2 = "123";
+		final String line1Address6 = "Ocean";
+		final String line1Address5 = "Dr";
+		final String line1Address3 = "Apt";
+		final String line1Address1 = "A";
+		final String countyDistrict1 = "Travis";
+		final String stateProvince1 = "Texas";
+		final String country1 = "US";
+		final String startDate1 = "2020-01-01 00:00:00";
+		final String endDate1 = "2020-12-31 00:00:00";
+		final String addressUuid1 = "address-uuid-1";
+		List<Object> personAddress1 = Arrays.asList(line1Address1, line1Address2, line1Address3, line1Address5,
+		    line1Address6, countyDistrict1, stateProvince1, country1, startDate1, endDate1, addressUuid1);
+		final String line2Address2 = "987";
+		final String line2Address6 = "Rubaga";
+		final String line2Address5 = "Rd";
+		final String line2Address3 = "Unit";
+		final String line2Address1 = "1";
+		final String countyDistrict2 = "Kampala";
+		final String stateProvince2 = "Central Region";
+		final String country2 = "Uganda";
+		final String startDate2 = "2009-01-01 00:00:00";
+		final String endDate2 = "2009-12-31 00:00:00";
+		final String addressUuid2 = "address-uuid-2";
+		List<Object> personAddress2 = Arrays.asList(line2Address1, line2Address2, line2Address3, line2Address5,
+		    line2Address6, countyDistrict2, stateProvince2, country2, startDate2, endDate2, addressUuid2);
+		List<List<Object>> addresses = new ArrayList();
+		addresses.add(personAddress1);
+		addresses.add(personAddress2);
+		when(executeQuery(FhirUtils.ADDRESS_QUERY.replace(ID_PLACEHOLDER, patientId))).thenReturn(addresses);
 		
-		when(mockPersonService.getPersonAttributeTypeByUuid(anyString())).thenReturn(new PersonAttributeType(1));
+		final String mobile = "123-456-7890";
+		final String attributeUuid1 = "attr-uuid-1";
+		final Integer mobileAttrTypeId = 1;
+		final String mobileAttrTypeUuid = "attr-type-uuid-1";
+		List<Object> mobileAttr = Arrays.asList(mobile, attributeUuid1);
+		when(mockAdminService.getGlobalProperty(MpiConstants.GP_PHONE_MOBILE)).thenReturn(mobileAttrTypeUuid);
+		PersonAttributeType mobileAttrType = new PersonAttributeType(mobileAttrTypeId);
+		when(mockPersonService.getPersonAttributeTypeByUuid(mobileAttrTypeUuid)).thenReturn(mobileAttrType);
+		when(executeQuery(
+		    ATTR_QUERY.replace(ID_PLACEHOLDER, patientId).replace(ATTR_TYPE_ID_PLACEHOLDER, mobileAttrTypeId.toString())))
+		            .thenReturn(singletonList(mobileAttr));
+		final String home = "098-765-4321";
+		final String attributeUuid2 = "attr-uuid-2";
+		final Integer homeAttrTypeId = 2;
+		final String homeAttrTypeUuid = "attr-type-uuid-2";
+		List<Object> homePhoneAttr = Arrays.asList(home, attributeUuid2);
+		when(mockAdminService.getGlobalProperty(MpiConstants.GP_PHONE_HOME)).thenReturn(homeAttrTypeUuid);
+		PersonAttributeType homeAttrType = new PersonAttributeType(homeAttrTypeId);
+		when(mockPersonService.getPersonAttributeTypeByUuid(homeAttrTypeUuid)).thenReturn(homeAttrType);
+		when(executeQuery(
+		    ATTR_QUERY.replace(ID_PLACEHOLDER, patientId).replace(ATTR_TYPE_ID_PLACEHOLDER, homeAttrTypeId.toString())))
+		            .thenReturn(singletonList(homePhoneAttr));
 		
 		Map<String, Object> resource = FhirUtils.buildPatient("1", patientVoided, personDetails, null);
 		
@@ -107,22 +142,73 @@ public class FhirUtilsTest {
 		assertEquals(!patientVoided, resource.get(MpiConstants.FIELD_ACTIVE));
 		assertEquals(birthDate, resource.get(MpiConstants.FIELD_BIRTHDATE));
 		assertEquals(dead, resource.get(MpiConstants.FIELD_DECEASED));
+		
 		List<Map> resourceIds = (List) resource.get(MpiConstants.FIELD_IDENTIFIER);
 		assertEquals(3, resourceIds.size());
 		assertEquals(MpiConstants.SOURCE_ID_URI, resourceIds.get(0).get(MpiConstants.FIELD_SYSTEM));
 		assertEquals(patientUuid, resourceIds.get(0).get(MpiConstants.FIELD_VALUE));
 		assertEquals(MpiConstants.SYSTEM_PREFIX + idTypeUuid1, resourceIds.get(1).get(MpiConstants.FIELD_SYSTEM));
 		assertEquals(identifier1, resourceIds.get(1).get(MpiConstants.FIELD_VALUE));
-		assertEquals(idUuid, resourceIds.get(1).get(MpiConstants.FIELD_ID));
+		assertEquals(idUuid1, resourceIds.get(1).get(MpiConstants.FIELD_ID));
+		assertEquals(MpiConstants.SYSTEM_PREFIX + idTypeUuid2, resourceIds.get(2).get(MpiConstants.FIELD_SYSTEM));
+		assertEquals(identifier2, resourceIds.get(2).get(MpiConstants.FIELD_VALUE));
+		assertEquals(idUuid2, resourceIds.get(2).get(MpiConstants.FIELD_ID));
+		
 		List<Map> resourceNames = (List) resource.get(MpiConstants.FIELD_NAME);
 		assertEquals(2, resourceNames.size());
 		assertEquals(MpiConstants.USE_OFFICIAL, resourceNames.get(0).get(MpiConstants.FIELD_USE));
 		assertEquals(prefix1, resourceNames.get(0).get(MpiConstants.FIELD_PREFIX));
 		assertEquals(nameUuid1, resourceNames.get(0).get(MpiConstants.FIELD_ID));
 		assertEquals(familyName1, resourceNames.get(0).get(MpiConstants.FIELD_FAMILY));
-		List<Object> givenNames = (List) resourceNames.get(0).get(MpiConstants.FIELD_GIVEN);
-		assertEquals(givenName1, givenNames.get(0));
-		assertEquals(middleName1, givenNames.get(1));
+		List<Object> givenNames1 = (List) resourceNames.get(0).get(MpiConstants.FIELD_GIVEN);
+		assertEquals(givenName1, givenNames1.get(0));
+		assertEquals(middleName1, givenNames1.get(1));
+		assertNull(resourceNames.get(1).get(MpiConstants.FIELD_USE));
+		assertEquals(prefix2, resourceNames.get(1).get(MpiConstants.FIELD_PREFIX));
+		assertEquals(nameUuid2, resourceNames.get(1).get(MpiConstants.FIELD_ID));
+		assertEquals(familyName2, resourceNames.get(1).get(MpiConstants.FIELD_FAMILY));
+		List<Object> givenNames2 = (List) resourceNames.get(1).get(MpiConstants.FIELD_GIVEN);
+		assertEquals(givenName2, givenNames2.get(0));
+		assertEquals(middleName2, givenNames2.get(1));
+		
+		List<Map> resourceAddresses = (List) resource.get(MpiConstants.FIELD_ADDRESS);
+		assertEquals(2, resourceAddresses.size());
+		List<Object> line1 = (List) resourceAddresses.get(0).get(MpiConstants.FIELD_LINE);
+		assertEquals(line1Address2, line1.get(0));
+		assertEquals(line1Address6, line1.get(1));
+		assertEquals(line1Address5, line1.get(2));
+		assertEquals(line1Address3, line1.get(3));
+		assertEquals(line1Address1, line1.get(4));
+		Map period1 = (Map) resourceAddresses.get(0).get(MpiConstants.FIELD_PERIOD);
+		assertEquals(DATETIME_FORMATTER.format(Timestamp.valueOf(startDate1)), period1.get(MpiConstants.FIELD_START));
+		assertEquals(DATETIME_FORMATTER.format(Timestamp.valueOf(endDate1)), period1.get(MpiConstants.FIELD_END));
+		assertEquals(countyDistrict1, resourceAddresses.get(0).get(MpiConstants.FIELD_DISTRICT));
+		assertEquals(stateProvince1, resourceAddresses.get(0).get(MpiConstants.FIELD_STATE));
+		assertEquals(country1, resourceAddresses.get(0).get(MpiConstants.FIELD_COUNTRY));
+		assertEquals(addressUuid1, resourceAddresses.get(0).get(MpiConstants.FIELD_ID));
+		List<Object> line2 = (List) resourceAddresses.get(1).get(MpiConstants.FIELD_LINE);
+		assertEquals(line2Address2, line2.get(0));
+		assertEquals(line2Address6, line2.get(1));
+		assertEquals(line2Address5, line2.get(2));
+		assertEquals(line2Address3, line2.get(3));
+		assertEquals(line2Address1, line2.get(4));
+		Map period2 = (Map) resourceAddresses.get(1).get(MpiConstants.FIELD_PERIOD);
+		assertEquals(DATETIME_FORMATTER.format(Timestamp.valueOf(startDate2)), period2.get(MpiConstants.FIELD_START));
+		assertEquals(DATETIME_FORMATTER.format(Timestamp.valueOf(endDate2)), period2.get(MpiConstants.FIELD_END));
+		assertEquals(countyDistrict2, resourceAddresses.get(1).get(MpiConstants.FIELD_DISTRICT));
+		assertEquals(stateProvince2, resourceAddresses.get(1).get(MpiConstants.FIELD_STATE));
+		assertEquals(country2, resourceAddresses.get(1).get(MpiConstants.FIELD_COUNTRY));
+		assertEquals(addressUuid2, resourceAddresses.get(1).get(MpiConstants.FIELD_ID));
+		
+		List<Map> resourceTelecoms = (List) resource.get(MpiConstants.FIELD_TELECOM);
+		assertEquals(MpiConstants.PHONE, resourceTelecoms.get(0).get(MpiConstants.FIELD_SYSTEM));
+		assertEquals(MpiConstants.MOBILE, resourceTelecoms.get(0).get(MpiConstants.FIELD_USE));
+		assertEquals(mobile, resourceTelecoms.get(0).get(MpiConstants.FIELD_VALUE));
+		assertEquals(attributeUuid1, resourceTelecoms.get(0).get(MpiConstants.FIELD_ID));
+		assertEquals(MpiConstants.PHONE, resourceTelecoms.get(1).get(MpiConstants.FIELD_SYSTEM));
+		assertEquals(MpiConstants.HOME, resourceTelecoms.get(1).get(MpiConstants.FIELD_USE));
+		assertEquals(home, resourceTelecoms.get(1).get(MpiConstants.FIELD_VALUE));
+		assertEquals(attributeUuid2, resourceTelecoms.get(1).get(MpiConstants.FIELD_ID));
 	}
 	
 }
