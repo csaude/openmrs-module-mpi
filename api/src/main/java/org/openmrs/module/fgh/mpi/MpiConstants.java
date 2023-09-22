26138c9ddda74eb230603502be49d20418704c58
wluyima <wyclif@openmrs.org>
2023-09-22 13:09:05 +0300

    Added global properties and constants for patient health center

--- api/src/main/java/org/openmrs/module/fgh/mpi/MpiConstants.java
+++ api/src/main/java/org/openmrs/module/fgh/mpi/MpiConstants.java
@@ -156,5 +156,9 @@ public class MpiConstants {
 	
 	public final static String GP_SANTE_MESSAGE_HEADER_EVENT_URI = MODULE_ID + ".sante.message.header.event.uri";
 	
+	public final static String GP_HEALTH_CENTER_SYSTEM_URI = MODULE_ID + ".health.center.id.system.uri";
+	
+	public final static String GP_HEALTH_CENTER_ENC_TYPE_UUID = MODULE_ID + ".health.center.encounter.type.uuid";
+	
 	public final static Range<Integer> HTTP_REQUEST_SUCCESS_RANGE = Range.between(200, 299);
 }
