a9e30df0eb2357787969a2ba97f010b39958542c
Daniel Chirinda <chirindzadaniel@gmail.com>
2025-04-09 14:15:10 +0200

    Fix issue trying to get global property

--- api/src/main/java/org/openmrs/module/fgh/mpi/processor/BaseEventProcessor.java
+++ api/src/main/java/org/openmrs/module/fgh/mpi/processor/BaseEventProcessor.java
@@ -66,6 +66,7 @@ public abstract class BaseEventProcessor {
 			Context.addProxyPrivilege(PrivilegeConstants.GET_RELATIONSHIP_TYPES);
 			Context.addProxyPrivilege(PrivilegeConstants.GET_PATIENTS);
 			Context.addProxyPrivilege(PrivilegeConstants.GET_ENCOUNTER_TYPES);
+			Context.addProxyPrivilege(PrivilegeConstants.GET_GLOBAL_PROPERTIES);
 			
 			switch (event.getTableName()) {
 				case "person":
@@ -99,6 +100,7 @@ public abstract class BaseEventProcessor {
 				Context.removeProxyPrivilege(PrivilegeConstants.GET_RELATIONSHIP_TYPES);
 				Context.removeProxyPrivilege(PrivilegeConstants.GET_PATIENTS);
 				Context.removeProxyPrivilege(PrivilegeConstants.GET_ENCOUNTER_TYPES);
+				Context.removeProxyPrivilege(PrivilegeConstants.GET_GLOBAL_PROPERTIES);
 			}
 			finally {
 				Context.closeSession();
